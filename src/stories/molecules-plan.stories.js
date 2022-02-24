185ffc4e68759e0228392643e09292e2c0a7dac0
Chinatsu Hasegawa <c.hasegawa@4nonome.com>
2022-02-24 15:34:08 +0900

    molecules-planの作成

---
+++ src/stories/molecules-plan.stories.js
@@ -0,0 +1,6 @@
+import Molecule_Plan from "../components/molecules/plan/plan.html";
+import "../components/molecules/plan/plan.scss";
+
+export default { title: "Molecules/Plan" };
+
+export const  molecules = () => Molecule_Plan;
