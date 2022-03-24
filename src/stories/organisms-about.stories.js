e77b9456669e1a048e2ebd35e699de17c030f89f
Chinatsu Hasegawa <c.hasegawa@4nonome.com>
2022-03-24 22:35:39 +0900

    organisms-aboutの作成

---
+++ src/stories/organisms-about.stories.js
@@ -0,0 +1,6 @@
+import Organisms_About from "../components/organisms/about/about.html";
+import "../components/organisms/about/about.scss";
+
+export default { title: "Organisms/About" };
+
+export const Organisms = () => Organisms_About;
